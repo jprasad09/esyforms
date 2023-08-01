0650ffaf90a4c39c6cdad91323e3efdb9bb876c4
Prasad Jadhav <phjadhav09@gmail.com>
2023-08-01 18:01:34 +0530

    initial setup

---
+++ server/index.js
@@ -0,0 +1,40 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+// express app
+const app = express()
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// handling not found routes and errors
+app.use((req, res, next) => {
+  let err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+app.use((err, req, res, next) => {
+  return res.status(err.status || 500).json({
+    success: false,
+    error: {
+      message: err.message || 'Something went wrong.',
+    },
+  })
+})
+
+// connect to db
+mongoose.set('strictQuery', false)
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('connected to database and listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+})
